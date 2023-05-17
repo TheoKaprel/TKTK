f34d2cab391fb27f0155c533b49c8dcb77478eca
TheoKaprel <theo.kaprelian@creatis.insa-lyon.fr>
2023-05-17 14:01:30 +0200

    image info script

--- setup.py
+++ setup.py
@@ -30,7 +30,8 @@ setuptools.setup(
         'tktk_center_image.py',
         'tktk_rename_itk_image.py',
         'tktk_scatter_correction_dew.py',
-        'tktk_convert.py'
+        'tktk_convert.py',
+        'tktk_image_info.py'
         ]
     )
 
